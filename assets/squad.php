d08c14ddecdd0473ee01e3178901077a34e22890
Asad Chaudhary <asad.chaudhary@vmdomain.com>
2024-12-20 01:34:55 +0530

    logo changed

--- assets/squad.php
+++ assets/squad.php
@@ -48,7 +48,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>VM Cricket tournament</title>
+    <title>DY PATIL Cricket tournament</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
@@ -60,7 +60,7 @@
 <body>
     <div class="row wrapper wrapperone">
         <div class="col-md-7 hero-bg">
-            <img src="assets/images/vereigen_logo.png" alt="Vereigen Logo">
+            <img src="assets/images/dyp-logo.png" alt="DYP Logo">
         </div>
         <div class="col-md-5 main-form-section pt-5">
             <div class="form-view=">
